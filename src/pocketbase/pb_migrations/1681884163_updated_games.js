45f77427879a84bd51620bd802085940fa8d5458
Edmier <94007463+Edmier@users.noreply.github.com>
2023-04-19 02:03:17 -0400

    Fix api perms

---
+++ src/pocketbase/pb_migrations/1681884163_updated_games.js
@@ -0,0 +1,17 @@
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("4t3cu0um3z57egj")
+
+  collection.listRule = "@request.query.gameId = id || @request.auth.game.id = id || @request.query.lobbyId = lobby.lobbyId || @request.query.lobbyId = lobby"
+  collection.viewRule = "@request.query.gameId = id || @request.auth.game.id = id || @request.query.lobbyId = lobby.lobbyId || @request.query.lobbyId = lobby"
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("4t3cu0um3z57egj")
+
+  collection.listRule = null
+  collection.viewRule = null
+
+  return dao.saveCollection(collection)
+})
